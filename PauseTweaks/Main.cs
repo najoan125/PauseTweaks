766365bd35021f6ea3510a357b353cee938d2af7
My Name <tiaaa0012@gmail.com>
2022-01-30 12:22:19 +0900

    초기 커밋.

---
+++ PauseTweaks/Main.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace PauseTweaks
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Harmony harmony;
+        public static bool IsEnabled = false;
+        public static bool ispause = false;
+
+        public static void Setup(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            IsEnabled = value;
+
+            if (value)
+            {
+                //켜질때
+                harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                //꺼질때
+                harmony.UnpatchAll(modEntry.Info.Id);
+            }
+            return true;
+        }
+    }
+}
---
+++ PauseTweaks/PausePatches.cs
@@ -0,0 +1,69 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace PauseTweaks
+{
+    public static class PausePatches
+    {
+        public static bool noReturn = false;
+        [HarmonyPatch(typeof(Game),"Update")]
+        public static class GameUpdatePatch
+        {
+            public static void Prefix(Game __instance)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape) && !Main.ispause && Game.isPlaying)
+                {
+                    Main.ispause = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.Escape) && Main.ispause)
+                {
+                    __instance.pause.GetComponent<PauseUI>().Resume();
+                    noReturn = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.Q) && Main.ispause)
+                {
+                    __instance.pause.GetComponent<PauseUI>().Exit();
+                    Main.ispause = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.R) && Main.ispause)
+                {
+                    __instance.pause.GetComponent<PauseUI>().Retry();
+                    Main.ispause = false;
+                }
+
+                if (Input.GetKeyUp(KeyCode.Escape) && noReturn)
+                {
+                    noReturn = false;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PauseUI),"Resume")]
+        public static class PauseUIResumePatch
+        {
+            public static void Prefix()
+            {
+                Main.ispause = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(PauseUI),"Pause")]
+        public static class PauseUIPausePatch
+        {
+            public static bool Prefix()
+            {
+                if (noReturn)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
